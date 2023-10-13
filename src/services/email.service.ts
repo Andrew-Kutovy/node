9edd08defcef592b3cbc524cc38f3eec6ba16c3a
home <andrijkutovij5@gmail.com>
2023-10-13 17:28:20 +0300

    +/-

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -1,18 +1,35 @@
-import hbs from 'nodemailer-express-handlebars'
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
+
+import { configs } from "../configs/config";
 
 class EmailService {
-    private transporter;
+  private transporter;
 
-    constructor() {
-        this.transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'andrewkutovy237@gmail.com',
-                pass: ''
-            }
-        })
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: configs.NO_REPLY_EMAIL,
+        pass: configs.NO_REPLY_PASSWORD,
+      },
+    });
+  }
+
+  public async sendMail(email: string) {
+    try {
+      const info = await this.transporter.sendMail({
+        from: "No reply", // Добавлено указание отправителя
+        to: email,
+        subject: "SUBJECT",
+        html: "<div>MY FIRST EMAIL</div>",
+      });
+      console.log("Email sent: ", info.messageId);
+      return info.messageId;
+    } catch (error) {
+      console.error("Error sending email: ", error);
+      throw new Error("Failed to send email");
     }
+  }
 }
 
-export const emailSer
+export const emailService = new EmailService();
